f4fb02337760fdf7b6a2946557f9c2d8bf2cfd8c
Tasneem <tasnim07@gmail.com>
2015-10-14 21:32:57 +0530

    changes in settings

--- BlackFly/settings.py
+++ BlackFly/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&!a9kjze+&b2gf=hbnk4rp%c(4+pqx28k!qd@x++k$r4nwqbs#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'BlackFly.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-"""
+
 try:
     from local_settings import DATABASES
 except ImportError:
     import dj_database_url
     DATABASES = {}
     DATABASES['default'] =  dj_database_url.config()
-"""
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
@@ -100,23 +100,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/blog/'
 LOGIN_URL = 'django.contrib.auth.views.login'
-
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
